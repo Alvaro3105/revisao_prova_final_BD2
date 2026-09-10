e2e8440e505be19e24c1597c7111b4e15e84f37f
Alvaro Pires de Souza <ap4321316@gmail.com>
2026-09-10 15:19:11 -0300

    Corrigir prova final 06 B

--- provas/Prova_Final_06_B.sql
+++ provas/Prova_Final_06_B.sql
@@ -1,145 +1,133 @@
-create or replace view vw_faturamento_clientes_emp as
-select
-    e.employeenumber,
-    e.firstname,
-    e.lastname,
-    count(distinct c.customernumber) as qtd_clientes,
-    coalesce(sum(p.amount), 0) as total_recebido
-from employees e
-inner join customers c
-    on e.employeenumber = c.salesrepemployeenumber
-left join payments p
-    on c.customernumber = p.customernumber
-where e.reportsto = 1143
-  and c.creditlimit > 100000
-group by
-    e.employeenumber,
-    e.firstname,
-    e.lastname;
+USE classicmodels;
 
+CREATE OR REPLACE VIEW vw_faturamento_clientes_emp AS
+SELECT
+    e.employeeNumber,
+    e.firstName,
+    e.lastName,
+    COUNT(DISTINCT c.customerNumber) AS qtd_clientes,
+    COALESCE(SUM(p.amount), 0) AS total_recebido
+FROM employees e
+INNER JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
+LEFT JOIN payments p ON c.customerNumber = p.customerNumber
+WHERE e.reportsTo = 1143
+  AND c.creditLimit > 100000
+GROUP BY e.employeeNumber, e.firstName, e.lastName;
 
-create or replace view vw_faturamento_clientes_emp_v2 as
-select
-    e.employeenumber,
-    e.firstname,
-    e.lastname,
-    c.customernumber,
-    coalesce(sum(p.amount), 0) as total_recebido
-from employees e
-inner join customers c
-    on e.employeenumber = c.salesrepemployeenumber
-left join payments p
-    on c.customernumber = p.customernumber
-where e.reportsto = 1143
-  and c.creditlimit > 100000
-group by
-    e.employeenumber,
-    e.firstname,
-    e.lastname,
-    c.customernumber;
+CREATE OR REPLACE VIEW vw_faturamento_clientes_emp_v2 AS
+SELECT
+    e.employeeNumber,
+    e.firstName,
+    e.lastName,
+    c.customerNumber,
+    COALESCE(SUM(p.amount), 0) AS total_recebido
+FROM employees e
+INNER JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
+LEFT JOIN payments p ON c.customerNumber = p.customerNumber
+WHERE e.reportsTo = 1143
+  AND c.creditLimit > 100000
+GROUP BY e.employeeNumber, e.firstName, e.lastName, c.customerNumber;
 
-with cte_vendedores as (
-    select
-        employeenumber,
-        firstname,
-        lastname,
-        customernumber,
-        total_recebido
-    from vw_faturamento_clientes_emp_v2
+WITH cte_vendas AS (
+    SELECT
+        v.employeeNumber,
+        v.firstName,
+        v.lastName,
+        v.customerNumber,
+        v.total_recebido,
+        COUNT(DISTINCT o.orderNumber) AS total_pedidos_cliente,
+        COALESCE(SUM(od.quantityOrdered), 0) AS total_produtos_cliente
+    FROM vw_faturamento_clientes_emp_v2 v
+    INNER JOIN orders o ON v.customerNumber = o.customerNumber
+    INNER JOIN orderdetails od ON o.orderNumber = od.orderNumber
+    WHERE o.orderDate >= '2003-01-01'
+      AND o.orderDate < '2003-04-01'
+    GROUP BY
+        v.employeeNumber,
+        v.firstName,
+        v.lastName,
+        v.customerNumber,
+        v.total_recebido
 )
-select
-    v.employeenumber,
-    v.firstname,
-    v.lastname,
-    sum(v.total_recebido) as total_recebido,
-    count(distinct o.ordernumber) as total_pedidos,
-    coalesce(sum(od.quantityordered), 0) as total_produtos_vendidos
-from cte_vendedores v
-inner join orders o
-    on v.customernumber = o.customernumber
-inner join orderdetails od
-    on o.ordernumber = od.ordernumber
-where o.orderdate between '2003-01-01' and '2003-03-31'
-group by
-    v.employeenumber,
-    v.firstname,
-    v.lastname;
+SELECT
+    employeeNumber,
+    firstName,
+    lastName,
+    SUM(total_recebido) AS total_recebido,
+    SUM(total_pedidos_cliente) AS total_pedidos,
+    SUM(total_produtos_cliente) AS total_produtos_vendidos
+FROM cte_vendas
+GROUP BY employeeNumber, firstName, lastName;
 
+CREATE OR REPLACE VIEW vw_analise_produtos AS
+SELECT
+    p.productCode,
+    FORMAT(((p.MSRP / p.buyPrice) - 1) * 100, 2) AS margem,
+    FORMAT((SUM(od.quantityOrdered) / (SUM(od.quantityOrdered) + p.quantityInStock)) * 100, 2) AS percentualVendido,
+    ((p.MSRP / p.buyPrice) - 1) * 100 AS margem_num,
+    (SUM(od.quantityOrdered) / (SUM(od.quantityOrdered) + p.quantityInStock)) * 100 AS percentualVendido_num
+FROM products p
+INNER JOIN orderdetails od ON p.productCode = od.productCode
+GROUP BY p.productCode, p.MSRP, p.buyPrice, p.quantityInStock;
 
-create or replace view vw_analise_produtos as
-select
-    p.productcode,
-    format(((p.msrp / p.buyprice) - 1) * 100, 2) as margem,
-    format((sum(od.quantityordered) / (sum(od.quantityordered) + p.quantityinstock) * 100), 2) as percentualvendido,
-    ((p.msrp / p.buyprice) - 1) * 100 as margem_num,
-    (sum(od.quantityordered) / (sum(od.quantityordered) + p.quantityinstock) * 100) as percentualvendido_num
-from products p
-inner join orderdetails od
-    on p.productcode = od.productcode
-group by
-    p.productcode,
-    p.msrp,
-    p.buyprice,
-    p.quantityinstock;
+DROP TABLE IF EXISTS analise_giro_margem;
 
-create table if not exists analise_giro_margem (
-    produto varchar(10),
-    observacao varchar(200)
+CREATE TABLE analise_giro_margem (
+    produto VARCHAR(15),
+    observacao VARCHAR(200)
 );
 
-delimiter //
-create procedure sp_analise_giro_margem()
-begin
-    truncate table analise_giro_margem;
+DROP PROCEDURE IF EXISTS sp_analise_giro_margem;
 
-    insert into analise_giro_margem (produto, observacao)
-    select
-        productcode as produto,
-        case
-            when margem_num >= 100 and percentualvendido_num > 20 then
-                'produto alto giro e excelente margem. manter preço'
-            when margem_num >= 100 and percentualvendido_num < 10 then
-                'produto baixo giro e excelente margem. reduzir o preço de venda'
-            when margem_num < 100 and percentualvendido_num > 20 then
-                'produto alto giro e baixa margem. aumentar o preço de venda'
-            else
-                'manter os valores praticados'
-        end as observacao
-    from vw_analise_produtos;
-end //
-delimiter ;
+DELIMITER //
+CREATE PROCEDURE sp_analise_giro_margem()
+BEGIN
+    TRUNCATE TABLE analise_giro_margem;
 
+    INSERT INTO analise_giro_margem (produto, observacao)
+    SELECT
+        productCode,
+        CASE
+            WHEN margem_num >= 100 AND percentualVendido_num > 20 THEN 'Produto alto GIRO e excelente Margem. Manter preço'
+            WHEN margem_num >= 100 AND percentualVendido_num < 10 THEN 'Produto baixo GIRO e excelente Margem. Reduzir o preço de venda'
+            WHEN margem_num < 100 AND percentualVendido_num > 20 THEN 'Produto alto GIRO e baixa Margem. Aumentar o preço de venda'
+            ELSE 'Manter os valores praticados'
+        END
+    FROM vw_analise_produtos;
+END //
+DELIMITER ;
 
-delimiter //
-create procedure sp_resumo_categoria(in p_categoria varchar(25))
-begin
-    select
-        c.name as categoria,
-        count(distinct f.film_id) as quantidade_filmes,
-        coalesce(sum(p.amount), 0) as valor_faturado
-    from category c
-    inner join film_category fc
-        on c.category_id = fc.category_id
-    inner join film f
-        on fc.film_id = f.film_id
-    inner join inventory i
-        on f.film_id = i.film_id
-    inner join rental r
-        on i.inventory_id = r.inventory_id
-    inner join payment p
-        on r.rental_id = p.rental_id
-    where c.name = p_categoria
-    group by c.name;
-end //
-delimiter ;
+USE sakila;
 
+DROP PROCEDURE IF EXISTS sp_resumo_categoria;
 
-create table if not exists falantes_china as
-select
-    cl.language as idioma,
-    sum(cl.percentage * c.population) / 100 as total_pessoas_falantes
-from countrylanguage cl
-inner join country c
-    on cl.countrycode = c.code
-where cl.countrycode = 'chn'
-group by cl.language;
+DELIMITER //
+CREATE PROCEDURE sp_resumo_categoria(IN p_categoria VARCHAR(25))
+BEGIN
+    SELECT
+        c.name AS categoria,
+        COUNT(DISTINCT f.film_id) AS quantidade_filmes,
+        COALESCE(SUM(p.amount), 0) AS valor_faturado
+    FROM category c
+    INNER JOIN film_category fc ON c.category_id = fc.category_id
+    INNER JOIN film f ON fc.film_id = f.film_id
+    LEFT JOIN inventory i ON f.film_id = i.film_id
+    LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+    LEFT JOIN payment p ON r.rental_id = p.rental_id
+    WHERE c.name = p_categoria
+    GROUP BY c.category_id, c.name;
+END //
+DELIMITER ;
+
+USE world;
+
+DROP TABLE IF EXISTS falantes_china;
+
+CREATE TABLE falantes_china AS
+SELECT
+    cl.Language AS idioma,
+    SUM(cl.Percentage * c.Population) / 100 AS total_pessoas_falantes
+FROM countrylanguage cl
+INNER JOIN country c ON cl.CountryCode = c.Code
+WHERE cl.CountryCode = 'CHN'
+GROUP BY cl.Language;
